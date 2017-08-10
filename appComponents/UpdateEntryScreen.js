3cdb29717b3254e7a0d43e2a761b28fb3f9c8e43
pbscrimmage <patrick@brooklynresearch.org>
2017-08-10 15:40:51 -0400

    camera area and icon in update screen

--- appComponents/UpdateEntryScreen.js
+++ appComponents/UpdateEntryScreen.js
@@ -4,6 +4,7 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  Image,
   View
 } from 'react-native';
 
@@ -17,7 +18,14 @@ export default class UpdateEntryScreen extends Component {
         const {params} = this.props.navigation.state
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>{params.doc.body}</Text>
+                <View style={styles.textArea}>
+                    <Text style={styles.docText}>{params.doc.body}</Text>
+                </View>
+                <View style={styles.cameraPort}>
+                    <Image
+                        source={require('../img/ic_add_a_photo_white_48dp.png')}
+                    />
+                </View>
             </View>
         )
     }
@@ -28,9 +36,19 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#000"
     },
-    text: {
+    textArea: {
+        flex: 1,
+        backgroundColor: "#000"
+    },
+    docText: {
         fontSize: 40,
         color: "#fff"
+    },
+    cameraPort: {
+        flex: 2,
+        backgroundColor: "#999",
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
