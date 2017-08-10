4a9b5bc8d24d21171c0b71632840388dec1b08bb
pbscrimmage <patrick@brooklynresearch.org>
2017-08-10 16:46:59 -0400

    button to unlock edit mode in update

--- appComponents/UpdateEntryScreen.js
+++ appComponents/UpdateEntryScreen.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 
 import {
   StyleSheet,
-  Text,
+  TextInput,
   TouchableOpacity,
   Image,
   View
 } from 'react-native';
 
+const lockOpen = require('../img/ic_lock_open_white_24dp.png')
+const lockClosed = require('../img/ic_lock_white_24dp.png')
+
 export default class UpdateEntryScreen extends Component {
 
     constructor(props) {
         super(props)
+
+        this.state = {
+            editingText: false
+        }
+    }
+
+    toggleEdit() {
+        this.setState({editingText: !this.state.editingText})
+        this.refs.TextEdit.focus()
     }
 
     render() {
@@ -19,7 +31,16 @@ export default class UpdateEntryScreen extends Component {
         return (
             <View style={styles.container}>
                 <View style={styles.textArea}>
-                    <Text style={styles.docText}>{params.doc.body}</Text>
+                    <TextInput style={styles.docText}
+                        ref="TextEdit"
+                        value={params.doc.body}
+                        editable={this.state.editingText}
+                    />
+                    <TouchableOpacity style={styles.editBtn} onPress={this.toggleEdit.bind(this)}>
+                        <Image
+                            source={this.state.editingText ? lockOpen : lockClosed}
+                        />
+                    </TouchableOpacity>
                 </View>
                 <View style={styles.cameraPort}>
                     <Image
@@ -38,11 +59,20 @@ const styles = StyleSheet.create({
     },
     textArea: {
         flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         backgroundColor: "#000"
     },
     docText: {
-        fontSize: 40,
+        flex: 3,
+        fontSize: 24,
         color: "#fff"
+    },
+    editBtn: {
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        right: "5%",
+        flex: 1,
     },
     cameraPort: {
         flex: 2,
